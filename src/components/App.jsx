a74ab7d4d6250c24a148ae9ae837561b4f3d82f9
Vadilonische <vadilonische26@gmail.com>
2024-05-04 21:47:16 +0300

    first step

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Description from './Description';
+import Feedback from './Feedback';
+import Options from './Options';
+
+export default function App() {
+  return (
+    <div>
+      <Description />
+      <Options />
+      <Feedback />
+    </div>
+  );
+}
